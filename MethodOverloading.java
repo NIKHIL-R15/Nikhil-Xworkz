a7c240b597c089153268436ffc8e566272f6ce26
NIKHIL-R15 <nikhilr.xworkz@gmail.com>
2021-02-01 13:18:34 +0530

    Adding MethodOverloading type of Java Program

---
+++ MethodOverloading.java
@@ -0,0 +1,19 @@
+class Addition
+{
+public static void main(String a[]){
+	
+
+add(23,77);
+add(56,67,21);
+}
+public static void add(int x, int y)
+{
+int z = x+y;
+System.out.println(z);
+}
+public static void add(int x, int y, int q)
+{
+int z = x+y+q;
+System.out.println(z);
+}
+}
