2bfa4ed9b429b686894954a9abc39b737d343ccc
NIKHIL-R15 <78068127+NIKHIL-R15@users.noreply.github.com>
2021-02-01 07:20:45 +0530

    Adding 3 new methods type java program

---
+++ AdditionOfTwoNumbers.java
@@ -0,0 +1,15 @@
+class AdditionOfTwoNumbers
+{
+public static void main(String a[])
+{
+add (8933,4234);
+add (8,3);
+add (11,314);
+add (34,21);
+}
+public static void add(int a, int b)
+{
+int c = a+b;
+System.out.println(c);
+}
+}
---
+++ SolarSystem.java
@@ -0,0 +1,14 @@
+class SolarSystem
+{
+public static void main(String a[])
+{
+bindsPlanets();
+bindsPlanets();
+bindsPlanets();
+
+}
+public static void bindsPlanets()
+{
+System.out.println("The solar system holds 8 planets");
+}
+}
